64da5628a6ce0a711a054d6c0df1ff468ce841b8
Aerodos12 <weldon.henson97@gmail.com>
2021-06-22 18:38:07 -0400

    Node Editor

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "imgui"
   kind "StaticLib"
   language "C++"
+  cppdialect "C++17"
+  staticruntime "on"
 
   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -18,13 +20,21 @@ project "imgui"
       "imstb_truetype.h",
       "imgui_tables.cpp",
       "imgui_demo.cpp",
-
+      "crude_json.h",
+      "crude_json.cpp",
+      "imgui_bezier_math.h",
+      "imgui_bezier_math.inl",
+      "imgui_canvas.h",
+      "imgui_canvas.cpp",
+      "imgui_extra_math.h",
+      "imgui_extra_math.inl",
+      "imgui_node_editor.h",
+      "imgui_node_editor.cpp",
+      "imgui_node_editor_api.h",
+      "imgui_node_editor_api.cpp",
+      "imgui_node_editor_internal.h",
+      "imgui_node_editor_internal.inl"
   }
 
     filter "system:windows"
-    systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "On"
-
-  filter {"system:windows","configurations:Release"}
-        buildoptions "/MT"
+      systemversion "latest"
